a2cd72b2818d6e3dd6ae0f2542baffcd89606690
Marian Geissler <marian.geissler@stud.htwk-leipzig.de>
2019-05-10 18:43:49 +0200

    Grundstruktur, Maße Cube zu Punkten in Matrix,

--- Programm/src/Main.java
+++ Programm/src/Main.java
@@ -1,7 +1,18 @@
+import java.io.File;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        int[] dimensions = {1,2,3};
+        CubeGenerator myCube = new CubeGenerator(dimensions);
+        myCube.toMatrix(dimensions);
+        myCube.printMatrix(myCube.toMatrix(dimensions));
     }
+
+    private File FileCreator(String geometryStlData)
+    {
+        return null;
+    }
+    private void cmdMenu(){};
 }
