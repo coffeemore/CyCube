e46b2a1c5f730630446b85bb26c119c0945643b6
Marian Geissler <marian.geissler@stud.htwk-leipzig.de>
2019-05-11 15:16:33 +0200

    Neue Klasse als Dummy hinzugefuegt-Cylinder

---
+++ Programm/src/CylinderGenerator.java
@@ -0,0 +1,17 @@
+public class CylinderGenerator
+{
+    private int radius;
+    private int hight;
+
+    public CylinderGenerator(int radius, int hight)
+    {
+        this.radius = radius;
+        this.hight = hight;
+    }
+
+    public String createCylinder(int radius, int hight)
+    {
+        String stlResult = "";
+        return stlResult;
+    }
+}
